13d82e31238b4d1dd97e4c8b54598b2ee5f68f3d
Aodhan Walker <54781261+awebdragon@users.noreply.github.com>
2025-10-15 16:20:27 -0600

    project start - config set up for tailwind, theme settings and colors, initial app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import snakeLogo from '/favicon.ico'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-row width-full justify-center py-4 px-10">
+        <img src={reactLogo} className="logo react mr-2" alt="React logo" />
+        <img src={snakeLogo} className="logo snake" alt="Snake logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
+      <div className="max-w-7xl mx-auto p-6 bg-surface rounded-md">
+        Some stuff and things! The game board should probably have its own version of this div, so we can use it as reference.
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
-      <p>Here lies the eventuality of a game of snake. I have not decided whether to make it play itself yet or only be user controlled. But there will be snake.</p>
+       
     </>
   )
 }
